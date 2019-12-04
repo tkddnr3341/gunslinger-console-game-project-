0704081b7f6628f23671b18abcfa0ab5e8dca9ce
tkddnr3341 <tkddnr3341@naver.com>
2019-12-04 14:18:01 +0900

    final

--- final.cpp
+++ final.cpp
@@ -345,8 +345,13 @@ int info()
 {
 	int input;
 	system("cls");
-	printf("game information\n");
-	printf("click space to go back to title\n");
+	printf("\n\n");
+	printf("                                  [ 조작법 ]\n\n");
+	printf("                         이동: 왼쪽-'A' 오른쪽-'D'\n");
+	printf("                              총알: 스페이스바\n\n\n\n\n\n\n");
+	printf("                   아이템을 먹으면 타겟들이 모두 사라집니다.\n");
+	printf("                         개발자: 김상욱, 김예린 이민욱\n");
+	printf("                스페이스바를 누르면 메인화면으로 이동합니다.\n");
 	input = keyControl();
 	while (input != SUBMIT)
 	{
